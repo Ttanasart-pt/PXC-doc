7112e4de82f57f349e9a2c1948167de29d57eced
Tanasart <tanasart_pt@outlook.co.th>
2024-02-20 15:33:52 +0700

    node gen

---
+++ gen/gen.py
@@ -0,0 +1,117 @@
+import os
+import shutil
+from re import sub
+from enum import Enum
+
+class FileType(Enum):
+    FILE = 0
+    DIR  = 1
+    BACK = 2
+
+root = "content"
+templatePath = "template.html"
+
+with open(templatePath, "r") as f:
+    template = f.read()
+
+def space(s): # Replace _ with space and capitalize the first letter in each word
+    s = s.replace('_', ' ')
+    return s.title()
+
+def pathStrip(path):
+    name = os.path.basename(path)
+    if name[0].isdigit():
+        name = name[name.find('_') + 1:]
+    return os.path.join(os.path.dirname(path), name)
+
+def loadFile(path):
+    with open(path, "r") as f:
+        return f.read()
+    
+svg_home = loadFile("src/svg/home.svg")
+svg_dir = loadFile("src/svg/dir.svg")
+
+def generateFile(path, sidebar):
+    with open(path, "r") as f:
+        content = f.read()
+
+    path      = pathStrip(path)
+    outPath   = f"docs/{path.replace(root, '').lower()}"
+    fileName  = os.path.basename(path).lower()
+
+    sideContent = ""
+    for s in sidebar:
+        fType = s[0]
+        fName = s[1]
+        title = s[2]
+
+        aClass  = ""
+        liClass = ""
+        icon    = ""
+
+        if fType == FileType.DIR:
+            icon = svg_dir
+
+        elif fType == FileType.FILE:
+            if fName == fileName :
+                liClass += "active "
+            if fName == "index.html":
+                icon = svg_home
+
+        elif fType == FileType.BACK:
+            liClass += "back "
+        
+        if icon != "":
+            liClass += "icon "
+
+        sideContent += f'<li class="{liClass}">{icon}<a class="{aClass}" href="{fName}">{title}</a></li>\n'
+
+    data = template.replace("{{content}}", content)
+    data = data.replace("{{sidebar}}", sideContent)
+
+    with open(outPath, "w") as f:
+        f.write(data)
+
+def generateFolder(path, parent = ""):
+    files = os.listdir(path)
+    sidebar = []
+
+    if parent == "":
+        groupTitle = "Home"
+        sidebar.append((FileType.BACK, "", ""))
+    else:
+        groupTitle = os.path.basename(path)
+        groupTitle = pathStrip(space(groupTitle))
+        sidebar.append((FileType.BACK, "../", "Back"))
+    
+    dirPath = f"docs/{path.replace(root, '')}"
+    if not os.path.exists(dirPath):
+        os.mkdir(dirPath)
+
+    for f in files:
+        fullPath = os.path.join(path, f)
+        f = pathStrip(f)
+
+        if os.path.isdir(fullPath):
+            title = space(f)
+            sidebar.append((FileType.DIR, f.lower(), title))
+
+        elif fullPath.endswith(".html"):
+            title = f.replace('.html', '')
+            title = space(title)
+            if title == "Index":
+                title = groupTitle
+
+            sidebar.append((FileType.FILE, f.lower(), title))
+
+    for f in files:
+        fullPath = os.path.join(path, f)
+
+        if os.path.isdir(fullPath):
+            generateFolder(fullPath, path)
+        elif fullPath.endswith(".html"):
+            generateFile(fullPath, sidebar)
+
+generateFolder(root)
+
+shutil.copy("styles.css", "docs/styles.css")
---
+++ gen/node.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+import re
+
+regPath = "D:\\Project\\MakhamDev\\LTS-PixelComposer\\PixelComposer\\scripts\\node_registry\\node_registry.gml"
+nodeDir = "D:\\Project\\MakhamDev\\LTS-PixelComposer\\PixelComposer\\scripts"
+
+content = ""
+with open(regPath, "r") as file:
+    content = file.read()
+
+nodeListRaws = content.split("// NODE LIST")
+nodeListRaw = nodeListRaws[1]
+
+cat   = ""
+nodes = {}
+
+def writeNodeFile(cat, node):
+    path = f"{nodeDir}\\{node}\\{node}.gml"
+
+    if not os.path.exists(path):
+        print(f"File not found: {path}")
+        return
+    
+    with open(path, "r") as file:
+        content = file.read()
+
+    nodeName  = node
+    className = node
+
+    match = re.search(r"name\s*=\s*['\"](.*)['\"]", content)
+    if match:
+        nodeName = match.group(1)
+    
+    txt = f"""<h1>{nodeName}</h1>
+<p class="subtitle">{className}</p>"""
+
+    filePath = f"content/nodes/{cat}/{className.replace('Node_', '')}.html"
+    
+    with open(filePath, "w") as file:
+        file.write(txt)
+
+for line in nodeListRaw.split("\n"):
+    line = line.strip()
+
+    if line.startswith("addNodeCatagory("):
+        cat = line.split("\"")[1]
+        nodes[cat] = []
+    elif line.startswith("addNodeObject("):
+        nodeClass = line.split(",")[3].strip().strip("\"")
+        nodes[cat].append(nodeClass)
+
+for cat in nodes:
+    catPath = f"content/nodes/{cat}"
+    if not os.path.exists(catPath):
+        os.makedirs(catPath)
+
+    txt = f"""<h1>{cat}</h1>"""
+    filePath = f"content/nodes/{cat}/0_index.html"
+    with open(filePath, "w") as file:
+        file.write(txt)
+
+    for node in nodes[cat]:
+        writeNodeFile(cat, node)
